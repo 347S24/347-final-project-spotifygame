cd8b3d8f52f618920a0aa033ef69d2f0aa2f3e82
mshare <sharemasha@gmail.com>
2024-04-09 15:21:36 -0400

    Fixed authenitcation problem

--- spotifyauth/urls.py
+++ spotifyauth/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name="pages/home.html"),
         name='home'
     ),
-    path('get-auth-url', AuthURL.as_view()),
+    path('get-auth-url', AuthURL.as_view(), name='get-auth-url'),
     path('redirect', spotify_callback),
     path('is-authenticated', IsAuthenticated.as_view()),
     path('game/', UserSavedTracks.as_view(), name='random_track')
