88fb459e8da6b8fe5eed947d42378c3e890b8f21
Megan Hinkel <hinkelme@dukes.jmu.edu>
2024-04-15 18:06:17 -0400

    game

---
+++ spotifygame/static/js/game.js
@@ -0,0 +1,7 @@
+function getGuess() {
+    // Get the value from the input field
+    var inputValue = document.getElementById("guess").value;
+    
+    // Do something with the value, for example, log it to the console
+    console.log("Input value:", inputValue);
+}
