9800b13264ec61559d4f34f7bfeac1bd3bdffaf3
Megan Hinkel <hinkelme@dukes.jmu.edu>
2024-04-16 12:24:49 -0400

    game

--- spotifygame/static/js/game.js
+++ spotifygame/static/js/game.js
@@ -1,7 +1,14 @@
-function getGuess() {
+document.getElementById("submit-button").addEventListener("click", function () {
     // Get the value from the input field
     var inputValue = document.getElementById("guess").value;
-    
     // Do something with the value, for example, log it to the console
     console.log("Input value:", inputValue);
-}
+});
+
+document.getElementById("hint-button").addEventListener("click", function () {
+    var hintButton = document.getElementById("hint-button");
+    var elem = document.createElement("<div id='currhint'> Hint goes here: </div>");
+    hintButton.appendChild(elem);
+});
+
+console.log("hi");
